d6d6b8d0c556872d022a282a651b440d8d4f18e1
njblore <njblore@outlook.com>
2019-05-10 14:28:29 +0100

    add method in user_info to post user info to database after account creation

--- lib/user_info.dart
+++ lib/user_info.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'auth.dart';
-import 'homepage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserInfo extends StatefulWidget {
   UserInfo({this.auth, this.onSignedOut});
@@ -12,61 +12,82 @@ class UserInfo extends StatefulWidget {
 }
 
 class _UserInfoState extends State<UserInfo> {
-  bool _filledOutDetails = false;
   String _name;
   String _dateofbirth;
   String _gender;
   List _hobbies;
 
+  final _formKey = GlobalKey<FormState>();
+
+  bool validateAndSave() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void validateAndSubmit() {
+    if (validateAndSave()) {
+      try {
+        //post name dob gender hobbies to db
+        widget.auth.currentUser().then((userId) {
+          Firestore.instance.collection('users').document(userId).updateData(
+              {"name": _name, "dob": _dateofbirth, "gender": _gender});
+        });
+      } catch (e) {
+        //error
+        print('Error: $e');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    switch (_filledOutDetails) {
-      case false:
-        return Scaffold(
-          appBar: AppBar(title: Text("Details")),
-          body: Center(
-            child: Container(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Form(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'name',
-                        ),
-                        onSaved: (value) => _name = value,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'gender',
-                        ),
-                        onSaved: (value) => _gender = value,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'date_ of birth',
-                        ),
-                        onSaved: (value) => _dateofbirth = value,
-                      ),
-                      RaisedButton(
-                          child: Text('Done!'),
-                          onPressed: () => setState(() {
-                                _filledOutDetails = true;
-                              }))
-                    ],
+    return Scaffold(
+      appBar: AppBar(title: Text("Details")),
+      body: Center(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text('Hello, who are you?'),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'name',
+                    ),
+                    validator: (value) =>
+                        value.isEmpty ? 'Please tell me your name!' : null,
+                    onSaved: (value) => _name = value,
                   ),
-                ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'gender',
+                    ),
+                    onSaved: (value) => _gender = value,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'date_ of birth',
+                    ),
+                    onSaved: (value) => _dateofbirth = value,
+                  ),
+                  RaisedButton(
+                      child: Text('Done!'),
+                      onPressed: () => {
+                            validateAndSubmit(),
+                          })
+                ],
               ),
             ),
           ),
-        );
-      case true:
-        return HomePage(
-          auth: widget.auth,
-          onSignedOut: widget.onSignedOut,
-        );
-    }
+        ),
+      ),
+    );
   }
 }
