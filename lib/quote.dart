0c57abc4578291db5192d9591c2fafe182e72519
njblore <njblore@outlook.com>
2019-05-14 17:24:45 +0100

    align quote to center and author to right

--- lib/quote.dart
+++ lib/quote.dart
@@ -7,7 +7,7 @@ class Quote extends StatelessWidget {
       'quote': 'Nothing diminishes anxiety faster than action.',
       'author': '- Walter Anderson'
     },
-    {'quote': 'Smile, breathe, and go slowly.', 'author': 'Thich Nhat Hanh'},
+    {'quote': 'Smile, breathe, and go slowly.', 'author': '- Thich Nhat Hanh'},
     {
       'quote':
           'You don’t have to control your thoughts. You just have to stop letting them control you.',
@@ -26,29 +26,40 @@ class Quote extends StatelessWidget {
     final quoteIndex = random.nextInt(3);
     return Container(
       margin: const EdgeInsets.all(20.0),
-      padding: const EdgeInsets.symmetric(vertical: 10),
+      padding: const EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-          color: Colors.pink,
-          borderRadius: BorderRadius.circular(8.0),
+          color: Colors.purple[100],
+          borderRadius: BorderRadius.circular(25.0),
           boxShadow: [
             BoxShadow(
-              color: Colors.purple,
+              color: Colors.pink[200],
               offset: Offset(0, 0),
-              blurRadius: 5,
+              blurRadius: 10,
             )
           ]),
       height: 150,
-      child:
-          Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        Text(quotes[quoteIndex]['quote'],
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontFamily: 'Tangerine', color: Colors.white, fontSize: 30)),
-        Text(quotes[quoteIndex]['author'],
-            textAlign: TextAlign.right,
-            style: TextStyle(
-                fontFamily: 'Bad Script', color: Colors.white, fontSize: 15))
-      ]),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(quotes[quoteIndex]['quote'],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontFamily: 'Tangerine',
+                    color: Colors.white,
+                    fontSize: 30)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(quotes[quoteIndex]['author'],
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        fontFamily: 'Bad Script',
+                        color: Colors.white,
+                        fontSize: 15)),
+              ],
+            )
+          ]),
     );
   }
 }
