b23b653c755ec669acb65a7a0002a8184e0b479e
Rhydian Davies <Rhydian@A-device.local>
2019-05-14 10:38:00 +0100

    calendar functionality started

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar/flutter_calendar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class CalendarPage extends StatelessWidget {
   @override
@@ -12,69 +14,66 @@ class CalendarPage extends StatelessWidget {
   }
 }
 
-class CalendarViewApp extends StatelessWidget {
+class CalendarViewApp extends StatefulWidget {
+  @override
+  _CalendarViewAppState createState() => _CalendarViewAppState();
+}
+
+class _CalendarViewAppState extends State<CalendarViewApp> {
+  String mood = '';
+  String activity = '';
+  String currentUser = '';
+
+  Future<String> getCurrentUser() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    return user.uid;
+  }
+
+  initState() {
+    getCurrentUser().then((userId) {
+      currentUser = userId;
+    });
+    super.initState();
+  }
+
   void handleNewDate(date) {
     print("handleNewDate $date");
+    Firestore.instance
+        .collection('users')
+        .document(currentUser)
+        .collection('history')
+        .document('2019-05-13')
+        .get()
+        .then((DocumentSnapshot ds) {
+      setState(() {
+        mood = ds['mood'];
+        activity = ds['activity'];
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 5.0,
+        vertical: 10.0,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Calendar'),
-        ),
-        body: Container(
-          margin: EdgeInsets.symmetric(
-            horizontal: 5.0,
-            vertical: 10.0,
+      child: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Calendar(
+            onSelectedRangeChange: (range) =>
+                print("Range is ${range.item1}, ${range.item2}"),
+            onDateSelected: (date) => handleNewDate(date),
+            isExpandable: true,
           ),
-          child: ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              Text('The Default Calendar:'),
-              Calendar(
-                onSelectedRangeChange: (range) =>
-                    print("Range is ${range.item1}, ${range.item2}"),
-                onDateSelected: (date) => handleNewDate(date),
-              ),
-              Divider(
-                height: 50.0,
-              ),
-              Text('The Expanded Calendar:'),
-              Calendar(
-                onSelectedRangeChange: (range) =>
-                    print("Range is ${range.item1}, ${range.item2}"),
-                isExpandable: true,
-              ),
-              Divider(
-                height: 50.0,
-              ),
-              Text('A Custom Weekly Calendar:'),
-              Calendar(
-                onSelectedRangeChange: (range) =>
-                    print("Range is ${range.item1}, ${range.item2}"),
-                isExpandable: true,
-                dayBuilder: (BuildContext context, DateTime day) {
-                  return InkWell(
-                    onTap: () => print("OnTap $day"),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black38)),
-                      child: Text(
-                        day.day.toString(),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ],
+          Divider(
+            height: 50.0,
           ),
-        ),
+          Text(mood),
+          Text(activity)
+        ],
       ),
     );
   }
