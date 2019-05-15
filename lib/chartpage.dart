a31cd1cff632bbcdd9cd7d37d12b058c33a7d7d6
longlandg <longlandg@hotmail.com>
2019-05-15 14:02:43 +0100

    chart gets data from firebase and creates a chart

--- lib/chartpage.dart
+++ lib/chartpage.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MoodOverTime extends StatefulWidget {
   @override
   _MoodOverTimeState createState() => _MoodOverTimeState();
+}
+
+Future<String> getCurrentUser() async {
+  FirebaseUser user = await FirebaseAuth.instance.currentUser();
+  return user.uid;
 }
 
 class MoodLine {
@@ -17,68 +24,79 @@ class MoodLine {
 }
 
 class _MoodOverTimeState extends State<MoodOverTime> {
+  List<MoodLine> moodData = [];
+
   @override
+  void initState() {
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    var data = [
-      MoodLine('2019-05-10', 5, Colors.red),
-      MoodLine('2019-05-11', 2, Colors.yellow),
-      MoodLine('2019-05-12', 5, Colors.green),
-    ];
+    return StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance
+            .collection('users')
+            .document('Zcqa8zm0QfVwWuWeER1Ba0yZ1FR2')
+            .collection('history')
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          snapshot.data.documents.forEach((ds) => moodData
+              .add(MoodLine(ds.documentID, (ds.data['emoji']), Colors.red)));
 
-    var series = [
-      new charts.Series(
-        domainFn: (MoodLine clickData, _) => clickData.date,
-        measureFn: (MoodLine clickData, _) => clickData.mood,
-        colorFn: (MoodLine clickData, _) => clickData.color,
-        id: 'Clicks',
-        data: data,
-      ),
-    ];
-
-    var chart = charts.BarChart(
-      series,
-      animate: true,
-    );
-
-    var chartWidget = new Padding(
-      padding: new EdgeInsets.all(32.0),
-      child: new SizedBox(
-        height: 200.0,
-        child: chart,
-      ),
-    );
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Line Chart'),
-        ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('My Charts'),
-                Expanded(child: chartWidget),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    RaisedButton(
-                      onPressed: () => {print('im pressed')},
-                      child: Icon(Icons.pie_chart),
-                    ),
-                    RaisedButton(
-                      onPressed: () => {print('im pressed')},
-                      child: Icon(Icons.access_time),
-                    ),
-                    RaisedButton(
-                      onPressed: () => {print('im pressed')},
-                      child: Icon(Icons.mood),
-                    ),
-                  ],
-                ),
-              ],
+          var series = [
+            new charts.Series<MoodLine, String>(
+              domainFn: (MoodLine clickData, _) => clickData.date,
+              measureFn: (MoodLine clickData, _) => clickData.mood,
+              colorFn: (MoodLine clickData, _) => clickData.color,
+              id: 'Clicks',
+              data: moodData,
             ),
-          ),
-        ));
+          ];
+
+          var chart = charts.BarChart(
+            series,
+            animate: true,
+          );
+
+          var chartWidget = new Padding(
+            padding: new EdgeInsets.all(32.0),
+            child: new SizedBox(
+              height: 200.0,
+              child: chart,
+            ),
+          );
+          return Scaffold(
+              appBar: AppBar(
+                title: Text('My Charts'),
+              ),
+              body: Center(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('My Mood Over Time'),
+                      Expanded(child: chartWidget),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          RaisedButton(
+                            onPressed: () => {print('im pressed')},
+                            child: Icon(Icons.pie_chart),
+                          ),
+                          RaisedButton(
+                            onPressed: () => {print('im pressed')},
+                            child: Icon(Icons.access_time),
+                          ),
+                          RaisedButton(
+                            onPressed: () => {print('im pressed')},
+                            child: Icon(Icons.mood),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ));
+        });
   }
 }
