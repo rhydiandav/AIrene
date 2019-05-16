75d589e8f8f78594dbbec336e951a8eb6d82e452
njblore <45038878+njblore@users.noreply.github.com>
2019-05-16 09:27:59 +0100

    Update auth.dart

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class BaseAuth {
   Future<String> signInWithEmailAndPassword(String email, String password);
